9749fb5b849c65a6c8fd8c681cf619f7fb18473d
vivek singh tomar <vivek.singh2994@gmail.com>
2017-12-05 22:21:32 +0530

    added proper comments and other required pep8 changes

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,36 +4,59 @@
 import media
 import fresh_tomatoes
 
-TOY_STORY = media.Movie("Toy Story",
-                        "A story of a boy and his toys.",
-                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+# Toy Story movie: movie title, storyline, poster image and movie trailer
+TOY_STORY = media.Movie(
+    "Toy Story",
+    "A story of a boy and his toys.",
+    "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",     # NOQA
+    "https://www.youtube.com/watch?v=KYz2wyBy3kc"
+    )
 
-AVATAR = media.Movie("Avatar",
-                     "A marine on an alien planet.",
-                     "https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Avatar-Teaser-Poster.jpg/220px-Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+# Avatar movie: movie title, storyline, poster image and movie trailer
+AVATAR = media.Movie(
+    "Avatar",
+    "A marine on an alien planet.",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Avatar-Teaser-Poster.jpg/220px-Avatar-Teaser-Poster.jpg",     # NOQA
+    "https://www.youtube.com/watch?v=5PSNL1qE6VY"
+    )
 
-BEFORESUNRISE = media.Movie("Before Sunrise",
-                            ("A young man and woman meet on a train in Europe, and wind up"
-                            "spending one evening together in Vienna. Unfortunately,"
-                            "both know that this will probably be their only night together."),
-                            "https://upload.wikimedia.org/wikipedia/en/d/da/Before_Sunrise_poster.jpg",
-                            "https://www.youtube.com/watch?v=9v6X-Dytlko")
+# Before Sunrise movie: movie title, storyline, poster image and movie trailer
+BEFORESUNRISE = media.Movie(
+    "Before Sunrise",
+    "A young man and woman meet on a train in Europe, and wind up "
+    "spending one evening together in Vienna. Unfortunately, "
+    "both know that this will probably be their only night together.",
+    "https://upload.wikimedia.org/wikipedia/en/d/da/Before_Sunrise_poster.jpg",     # NOQA
+    "https://www.youtube.com/watch?v=9v6X-Dytlko"
+    )
 
-HACKSAWRIDGE = media.Movie("Hacksaw Ridge",
-                           ("WWII American Army Medic Desmond T. Doss, who served during the Battle"
-                           "of Okinawa, refuses to kill people, and becomes the first man in American"
-                           "history to receive the Medal of Honor without firing a shot."),
-                           "https://upload.wikimedia.org/wikipedia/en/thumb/8/8a/Hacksaw_Ridge_poster.png/220px-Hacksaw_Ridge_poster.png",
-                           "https://www.youtube.com/watch?v=s2-1hz1juBI")
+# Hacksaw Ridge movie: movie title, storyline, poster image and movie trailer
+HACKSAWRIDGE = media.Movie(
+    "Hacksaw Ridge",
+    "WWII American Army Medic Desmond T. Doss, who served during the Battle "
+    "of Okinawa, refuses to kill people, and becomes the first man "
+    "in American history to receive the Medal of Honor without firing a shot.",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/8/8a/Hacksaw_Ridge_poster.png/220px-Hacksaw_Ridge_poster.png",     # NOQA
+    "https://www.youtube.com/watch?v=s2-1hz1juBI"
+    )
 
-PELE = media.Movie("Pele: Birth of a legend",
-                   ("Pele's meteoric rise from the slums of Sao Paulo to leading"
-                   "Brazil to its first World Cup victory at the age of 17 is chronicled"
-                   "in this biographical drama."),
-                   "https://upload.wikimedia.org/wikipedia/en/thumb/d/df/Pel%C3%A9_%28film_poster%29.jpg/220px-Pel%C3%A9_%28film_poster%29.jpg",
-                   "https://www.youtube.com/watch?v=XBrfxHOXsDE")
+# Pele movie: movie title, storyline, poster image and movie trailer
+PELE = media.Movie(
+    "Pele: Birth of a legend",
+    "Pele's meteoric rise from the slums of Sao Paulo to leading "
+    "Brazil to its first World Cup victory at the age of 17 is chronicled "
+    "in this biographical drama.",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/d/df/Pel%C3%A9_%28film_poster%29.jpg/220px-Pel%C3%A9_%28film_poster%29.jpg",       # NOQA
+    "https://www.youtube.com/watch?v=XBrfxHOXsDE")
 
-MOVIES = [TOY_STORY, AVATAR, BEFORESUNRISE, HACKSAWRIDGE, PELE]
+# Set list of movies
+MOVIES = [
+    TOY_STORY,
+    AVATAR,
+    BEFORESUNRISE,
+    HACKSAWRIDGE,
+    PELE
+    ]
+
+# Create fresh_tomatoes.html file and open it in web-browser
 fresh_tomatoes.open_movies_page(MOVIES)
--- media.py
+++ media.py
@@ -3,6 +3,7 @@
 
 import webbrowser
 
+
 class Movie(object):
     """Class Movie to store movie details and show trailer
     Attributes:
@@ -12,7 +13,8 @@ class Movie(object):
         trailer_youtube: URL to the trailer of the movie.
     """
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
